1b552d20b093a82e66b8a849a6755b371269b741
Vikash Kumar <rajvikash18113@gmail.com>
2024-09-13 22:19:56 +0530

    Update lab4.cpp

--- DSA Lab/lab4.cpp
+++ DSA Lab/lab4.cpp
@@ -3,17 +3,23 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int array[], int low, int high, int x) {
-    while (low <= high) {
+int binarySearch(int array[], int low, int high, int x)
+{
+    while (low <= high)
+    {
         int mid = (low + high) / 2;
 
-        if (array[mid] == x) {
+        if (array[mid] == x)
+        {
             return mid; // Element found, return its index
         }
 
-        if (array[mid] < x) {
+        if (array[mid] < x)
+        {
             low = mid + 1; // Search in the right half
-        } else {
+        }
+        else
+        {
             high = mid - 1; // Search in the left half
         }
     }
@@ -21,15 +27,19 @@ int binarySearch(int array[], int low, int high, int x) {
     return -1; // Element not found
 }
 
-int main() {
+int main()
+{
     int arr[] = {2, 3, 5, 7, 9, 10}; // Sorted array
-    int x = 7; // Element to search
+    int x = 7;                       // Element to search
     int n = sizeof(arr) / sizeof(arr[0]);
     int result = binarySearch(arr, 0, n - 1, x);
 
-    if (result == -1) {
+    if (result == -1)
+    {
         cout << "Element not found" << endl;
-    } else {
+    }
+    else
+    {
         cout << "Element found at index " << result << endl;
     }
 
