f4f70aaf6f211d7a624e545dfd18ea87e69866e1
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-14 15:36:15 +0530

    Create lab15.cpp

---
+++ OOPs Lab/lab15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class A {
+private:
+ int a, b;
+public:
+ A(int x, int y) {
+ a = x;
+ b = y;
+ cout << "The values of a and b are: " <<endl;
+ cout << a <<endl;
+ cout << b << endl;
+ }
+void sum() {
+ cout << "sum is=" << a + b;
+ }
+};
+int main() {
+ A a1(20, 30);
+ cout << endl;
+ a1.sum();
+ return 0;
+}
