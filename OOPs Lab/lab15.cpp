bdeb1c1009d04f4c2805a56c59b7510f7561688f
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-14 15:36:29 +0530

    Update lab15.cpp

--- OOPs Lab/lab15.cpp
+++ OOPs Lab/lab15.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 using namespace std;
-class A {
+class A
+{
 private:
- int a, b;
+    int a, b;
+
 public:
- A(int x, int y) {
- a = x;
- b = y;
- cout << "The values of a and b are: " <<endl;
- cout << a <<endl;
- cout << b << endl;
- }
-void sum() {
- cout << "sum is=" << a + b;
- }
+    A(int x, int y)
+    {
+        a = x;
+        b = y;
+        cout << "The values of a and b are: " << endl;
+        cout << a << endl;
+        cout << b << endl;
+    }
+    void sum()
+    {
+        cout << "sum is=" << a + b;
+    }
 };
-int main() {
- A a1(20, 30);
- cout << endl;
- a1.sum();
- return 0;
+int main()
+{
+    A a1(20, 30);
+    cout << endl;
+    a1.sum();
+    return 0;
 }
