6d491142d5c4a5fa231dcdd6651c103926a7e09a
Vikash Kumar <rajvikash18113@gmail.com>
2024-11-22 18:53:49 +0530

    Create StringHandling2.java

---
+++ Java/StringHandling2.java
@@ -0,0 +1,13 @@
+public class StringHandling2 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hello");
+
+        System.out.println(s1 == s2);
+        System.out.println(s2 == s3);
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(s3));
+
+    }
+}
