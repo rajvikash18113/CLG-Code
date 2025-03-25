5a0aa53351e5da48aa1251fa0645751c6c1c55da
Vikash Kumar <rajvikash18113@gmail.com>
2025-03-25 22:47:43 +0530

    Create naturalnNumber.java

---
+++ 4th Sem/4th Sem Java/naturalnNumber.java
@@ -0,0 +1,16 @@
+public class naturalnNumber {
+    public static void main(String[] args) {
+        int n = 10;
+        // for(int i = 0; i <= n; i++)
+        // {
+        //     System.out.println(i);
+        // }
+
+        int i = 0;
+        while(i <= 100)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
