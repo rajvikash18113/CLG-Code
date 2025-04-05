5da3c93f69894a79dc0468621faf7eb1be6bf437
Vikash Kumar <rajvikash18113@gmail.com>
2025-04-05 19:32:45 +0530

    Create sum.java

---
+++ 4th Sem/4th Sem Java/sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sum{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b =  sc.nextInt();
+        System.out.println("Sum of both number is : " + (a + b));
+    }
+}
