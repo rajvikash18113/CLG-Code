45cfc1cd8def8c6414b2cf7a70f6af281a7dd691
Vikash Kumar <rajvikash18113@gmail.com>
2025-04-09 19:59:10 +0530

    Create pattern2.java

---
+++ 4th Sem/4th Sem Java/pattern2.java
@@ -0,0 +1,23 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+
+import java.util.Scanner;
+public class pattern2 {
+    public static void main(String args[])
+    {
+        System.out.println("Enter the size of the pattern 2 : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
